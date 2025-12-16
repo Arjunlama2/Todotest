f98838d9dabde9c67d924ddff5adb54ca0ee1f09
Arjunlama2 <instructorarjun1@gmail.com>
2025-12-16 13:57:35 +0545

    inital todo

---
+++ index.js
@@ -0,0 +1,21 @@
+let Tasks=["data1","data2"]
+
+function getDataFromHtml(event) {
+    event.preventDefault()
+let task=event.target.task.value
+Tasks.push(task)
+displayTasks()
+}
+
+
+function displayTasks() {
+let ul=document.getElementById("task-list")
+let string=""
+Tasks.forEach((el)=>{
+string+=`<li>${el} <button>delete</button></li>`
+})
+ul.innerHTML=string
+}
+
+
+displayTasks()
