dee0ca0e8b23b3a9d13cb715ca459e6cabf67e14
Arjunlama2 <instructorarjun1@gmail.com>
2025-12-18 13:57:45 +0545

    updates

--- index.js
+++ index.js
@@ -1,33 +1,43 @@
-let Tasks= JSON.parse(localStorage.getItem("tasks")) || []
+let Tasks = JSON.parse(localStorage.getItem("tasks")) || []
 
 function getDataFromHtml(event) {
     event.preventDefault()
-let task=event.target.task.value
-Tasks.push(task)
-localStorage.setItem("tasks",JSON.stringify(Tasks))
-displayTasks()
+    let task = event.target.task.value
+    Tasks.push(task)
+    localStorage.setItem("tasks", JSON.stringify(Tasks))
+    displayTasks()
 }
-
 
 function displayTasks() {
-let ul=document.getElementById("task-list")
-let string=""
-Tasks.forEach((el,index)=>{
-string+=`<li>${el} <button onClick="deteleTask(${index})"><span class="material-symbols-outlined">
-delete
-</span></button></li>`
-})
-ul.innerHTML=string
+    
+    const ul = document.getElementById("list")
+    ul.innerHTML = "";
+    let string = "";
+
+    Tasks.forEach((el, index) => {
+        string += `
+        <li>
+            ${el}
+            <button onClick="deleteTask(${index})">
+            <span class="material-symbols-outlined">delete</span>
+            </button>
+        </li>`;
+    });
+
+    ul.innerHTML = string;
+
 }
 
 
+
 displayTasks()
 
 
-function deteleTask(index) {
-Tasks.splice(index,1)
-localStorage.setItem("tasks",JSON.stringify(Tasks))
-displayTasks()
+function deleteTask(index) {
+    Tasks.splice(index, 1)
+    localStorage.setItem("tasks", JSON.stringify(Tasks))
+    displayTasks()
+
 }
 
 //git init
@@ -36,10 +46,21 @@ displayTasks()
 // git remote add origin <repo-link>
 // git add .
 // git commit -m "first commit"
-// git push 
+// git push
 
 
 // now nextime
 // git add .
 // git commit -m "your message"
 // git push
+
+
+
+// let random=Math.random()*100
+// console.log(random)
+// console.log(Math.floor(random))
+// console.log(Math.ceil(random))
+// console.log(Math.round(random))
+
+
+
---
+++ test.js
@@ -0,0 +1,28 @@
+
+function test(){
+    console.log("from insde fn")
+
+
+}
+test()
+
+
+
+
+// const test2=()=>{
+//     console.log("from inside arrow fn")
+// }
+
+
+// test2()
+
+
+
+
+
+
+
+// hotiing 
+// recursive function 
+// differene between var,let and const
+// scope  wit varibles
